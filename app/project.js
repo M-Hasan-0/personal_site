7eac2f963be28221dc8d65e04b001d98e9b9f343
Mahedi <mahedi.hasan27610@gmail.com>
2024-04-03 21:32:25 +1100

    added-layout-t0-project-page

--- app/project.js
+++ app/project.js
@@ -4,16 +4,18 @@ import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.mi
 const project_array = window.localStorage.getItem("clicked_project");
 const project = JSON.parse(project_array)
 
-console.log(project[0]);
+// console.log(project[0]);
 
 const {name , img , doc} = project[0];
 
-const project_html = document.getElementById("project");
+// const project_html = document.getElementById("project");
+const project_heading = document.getElementById("project-name");
 
+project_heading.innerText = name;
 
-project_html.innerHTML = `
-                                                <h1>${name}</h1>
+// project_html.innerHTML = `
+//                                                 <h1>${name}</h1>
 
-                                                <a href="${doc}">Doc</a>
+//                                                 <a href="${doc}">Doc</a>
 
-                                                <a href="${img}">Model</a>`
+//                                                 <a href="${img}">Model</a>`
